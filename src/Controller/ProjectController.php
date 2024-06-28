57ab5acf8beb2e5063d49d0151c935c1966989bc
Simon-Honore <simon.honore6@gmail.com>
2024-06-28 17:04:47 +0200

    feat(projects): show one project page

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 #[Route('/project', name: 'project_')]
 class ProjectController extends AbstractController
@@ -17,5 +19,18 @@ class ProjectController extends AbstractController
         return $this->render('project/index.html.twig', [
             'projects' => $projects,
         ]);
+    }
+
+    #[Route('/{slug}-{id}', name: 'show', requirements: ['id' => Requirement::DIGITS, 'slug' => Requirement::ASCII_SLUG])]
+    public function show(Project $project, string $slug): Response
+    {
+        if ($project->getSlug() !== $slug) {
+            return $this->redirectToRoute('project_show', ['id' => $project->getId(), 'slug' => $project->getSlug()]);
+        }
+
+        return $this->render('project/show.html.twig', [
+            'project' => $project,
+        ]);
+
     }
 }
