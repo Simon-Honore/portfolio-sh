7bb67ed4dc49b6b541d60894fa05a855d93dc8ca
Simon-Honore <simon.honore6@gmail.com>
2024-06-28 11:01:05 +0200

    feat(admin): auth

--- src/Controller/admin/ProjectController.php
+++ src/Controller/admin/ProjectController.php
@@ -11,8 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/project', name: 'admin_project_')]
+#[IsGranted('ROLE_ADMIN')]
 class ProjectController extends AbstractController
 {
     #[Route('/', name: 'index')]
--- src/Controller/admin/TechnologyController.php
+++ src/Controller/admin/TechnologyController.php
@@ -11,8 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/technology', name: 'admin_technology_')]
+#[IsGranted('ROLE_ADMIN')]
 class TechnologyController extends AbstractController
 {
     #[Route('/', name: 'index')]
